d74ddc35d8a8dabe93feb8c9d18d5fcc7e98b4a4
sowmyanakkina <78066345+sowmyanakkina@users.noreply.github.com>
2021-02-01 15:49:48 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-fetch("../data/data.json").then(response=>{
+fetch("https://sowmyanakkina.github.io/ecommerce/data/data.json").then(response=>{
 	return response.json()
 }).then(data=>{
 	console.log(data)
